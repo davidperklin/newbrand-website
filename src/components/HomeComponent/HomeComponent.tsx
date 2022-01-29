e18a2a0877155c67c74d8aa6dc9b11072c666078
David Perklin <davidperklin@hotmail.com>
2022-01-29 17:13:54 -0500

    LAUNCH

--- src/components/HomeComponent/HomeComponent.tsx
+++ src/components/HomeComponent/HomeComponent.tsx
@@ -2,6 +2,7 @@ import { Container, FullScreenCentered } from '@components/common'
 import { Link } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 import React from 'react'
+import { Icon } from 'src/@ds'
 import { PRODUCT_PAGE_BASE_SLUG } from 'src/constants'
 
 const HomeComponent = ({ image, handle }) => {
@@ -21,6 +22,28 @@ const HomeComponent = ({ image, handle }) => {
           </Link>
         </div>
       </FullScreenCentered>
+      <div className="md:hidden">
+        <FullScreenCentered>
+          <ul className="w-full">
+            <li>
+              <Link
+                to="/sneakers?ref=mobile_home_link"
+                className="flex justify-between items-center w-full text-contrast hover:no-underline py-4 border-b"
+              >
+                Browse sneakers made in Italy <Icon name="ArrowRight" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/clothing?ref=mobile_home_link"
+                className="flex justify-between items-center w-full text-contrast hover:no-underline py-4"
+              >
+                Browse clothing made in Canada <Icon name="ArrowRight" />
+              </Link>
+            </li>
+          </ul>
+        </FullScreenCentered>
+      </div>
     </Container>
   )
 }
