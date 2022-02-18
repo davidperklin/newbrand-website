81ca08cc9d2634d98d376c56488c286852bf65fe
David Perklin <davidperklin@hotmail.com>
2022-02-17 21:42:14 -0500

    Added google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,5 +54,24 @@ module.exports = {
     },
     `gatsby-plugin-sass`,
     'gatsby-plugin-postcss',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'G-TXT3V9KZ21',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ],
 }
